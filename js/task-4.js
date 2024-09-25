bce29bbfebde9a54765eecc04d9a2873107ec4ed
Maryna Baziliuk <marina.mus24@gmail.com>
2024-09-25 18:47:58 +0300

    fix task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener("submit", getUserInput);
 
+function getUserInput(evt) {
+  evt.preventDefault();
+  const form = evt.currentTarget;
+  const { email, password } = form.elements;
+
+  if (email.value.trim() === "" || password.value.trim() === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const infoUser = {
+    email: email.value.trim(),
+    password: password.value.trim(),
+  };
+
+  console.log(infoUser);
+
+  formEl.reset();
+}
