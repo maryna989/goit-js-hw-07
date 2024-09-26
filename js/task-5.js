a741de6ab2ca771930e83539bd9f9586ef7cf7d1
Maryna Baziliuk <marina.mus24@gmail.com>
2024-09-26 12:17:56 +0300

    fix task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const container = document.querySelector(".widget");
+const spanText = document.querySelector(".color");
+const colorBody = document.querySelector("body");
+
+container.addEventListener("click", getColor);
+
+function getColor(evt) {
+  evt.preventDefault();
+
+  const newColor = getRandomHexColor();
+
+  colorBody.style.backgroundColor = newColor;
+
+  spanText.textContent = newColor;
+}
